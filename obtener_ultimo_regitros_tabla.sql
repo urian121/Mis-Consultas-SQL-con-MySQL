d9dac105eb26b91cef25c029778186d796dd6e80
URIAN VIERA <urian1213viera@gmail.com>
2023-10-20 08:16:46 -0500

    Add files via upload

--- obtener_ultimo_regitros_tabla.sql
+++ obtener_ultimo_regitros_tabla.sql
@@ -1,9 +1,12 @@
+-- Opción 1
 SELECT MAX(id) AS ultimo_id
 FROM consignaciones;
+-- Opción 2
 SELECT id
 FROM consignaciones
 ORDER BY id DESC
 LIMIT 1;
+-- Opción 3
 SELECT id
 FROM consignaciones
 WHERE id = (
