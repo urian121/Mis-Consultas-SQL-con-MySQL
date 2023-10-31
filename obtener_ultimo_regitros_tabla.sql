e714fab97200597928e45d0795b0ba6af91e0ef9
URIAN VIERA <urian1213viera@gmail.com>
2023-10-30 21:35:12 -0500

    Update obtener_ultimo_regitros_tabla.sql

--- obtener_ultimo_regitros_tabla.sql
+++ obtener_ultimo_regitros_tabla.sql
@@ -15,3 +15,15 @@ WHERE id = (
         SELECT MAX(id)
         FROM consignaciones
     );
+
+
+-- Para obtener el penúltimo registro de una tabla en SQL
+/**
+La cláusula OFFSET 1 en una consulta SQL se utiliza para saltar la primera fila (que sería el último registro en la tabla después de ordenarla en orden descendente). 
+El LIMIT 1 se utiliza para limitar el resultado a una sola fila, lo que significa que obtendrás la segunda fila del conjunto de resultados,
+que en este caso es el penúltimo registro de la tabla.
+*/
+SELECT *
+FROM tbl_users
+ORDER BY id_user DESC
+LIMIT 1 OFFSET 1;
